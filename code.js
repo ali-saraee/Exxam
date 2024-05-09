2ba0dfc5a3b4aca096a24f92646a903620258992
ali saraee <ghialibar@gmail.com>
2024-05-09 20:34:12 +0430

    disign

---
+++ code.js
@@ -0,0 +1,12 @@
+
+function nextItem(array){
+    let result=0
+    for(i=0;i<array.length;i++){
+        result+=array[i]
+    }
+    console.log(result)
+}
+nextItem([1,2,3,4])
+nextItem([5,6,2])
+
+
